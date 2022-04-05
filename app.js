4af3f0a0d6efae390aa214c744f8df204eccf9d0
Diana <dianagithubdavis@gmail.com>
2022-04-04 22:17:29 -0400

    Notes out references to array

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 console.log('connected')
 
 //an array of quotes
-const arrayOfQuotes = ["A few words.", "Something else.", "Words of wisdom."];
+//const arrayOfQuotes = ["A few words.", "Something else.", "Words of wisdom."];
 
 
 //function to grab a quote at random
-const pickAQuote = () => {
+/*const pickAQuote = () => {
     const randomIndex = Math.floor(Math.random() * arrayOfQuotes.length);
     let quote = arrayOfQuotes[randomIndex];
     console.log(quote);
     return quote;
 }
-
+*/
 //console.log(pickAQuote());
 document.getElementById('quoteButton').addEventListener('click', pickQuoteFromAPI);
 
